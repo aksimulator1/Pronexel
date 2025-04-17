11ded3a1da548d71a6d27040e455fe0726bb544b
mirmoheen9 <mirmoheen9@gmail.com>
2025-04-17 09:00:38 +0500

    user repo

---
+++ PRONEXEL_Business/Repositories/UserRepo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRONEXEL_Business.Repositories
+{
+    public class UserRepo
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AppDbContext _context;
+        public UserRepo(AppDbContext context, UserManager<IdentityUser> _userManager, RoleManager<IdentityRole> _roleManager, IHttpContextAccessor httpContextAccessor)
+        {
+
+            this._context = context;
+            this._userManager = _userManager;
+            this._roleManager = _roleManager;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task CreateRoles()
+        {
+            string[] roles = { "ApplicationUser", "Admin","SuperAdmin" }; // Add more roles as needed
+            foreach (var role in roles)
+            {
+                bool roleExists = await _roleManager.RoleExistsAsync(role);
+                if (!roleExists)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
--- PRONEXEL_WEB/Controllers/AccountController.cs
+++ PRONEXEL_WEB/Controllers/AccountController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PRONEXEL_WEB.Controllers
 {
 	public class AccountController : Controller
 	{
+		[AllowAnonymous]
 		public IActionResult Login()
 		{
 			return View();
 		}
-	}
+        public IActionResult CreateRoles()
+        {
+            return View();
+        }
+
+    }
 }
