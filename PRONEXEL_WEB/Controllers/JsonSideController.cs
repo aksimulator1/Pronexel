c824461137dabadf1f052db1e827c4f993ea7e41
mirmoheen9 <mirmoheen9@gmail.com>
2025-05-04 12:59:54 +0500

    user side

---
+++ PRONEXEL_WEB/Controllers/JsonSideController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRONEXEL_Business.Repositories;
+
+namespace PRONEXEL_WEB.Controllers
+{
+    public class JsonSideController : Controller
+    {
+        private readonly ContentRepo _contentRepo;
+        public JsonSideController(ContentRepo contentRepo)
+        {
+            _contentRepo = contentRepo;
+        }
+        [HttpGet]
+        public async Task<JsonResult> AllCategroy()
+        {
+            var data = await _contentRepo.GetTopic();
+            return Json(data);
+        }
+        [HttpGet]
+        public async Task<JsonResult> AllSubCategroy(string categoryId)
+        {
+            var cat = await _contentRepo.GetTopic();
+            var cid = cat.Where(x => x.ID == categoryId).FirstOrDefault();
+            var data = await _contentRepo.GetSubTopics();
+            if(data!=null)
+            {
+                data=data.Where(x=>x.TopicName== cid.TopicName).ToList();
+            }
+            return Json(data);
+        }
+    }
+}
---
+++ PRONEXEL_WEB/Controllers/TrainingModuleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PRONEXEL_Business.Repositories;
+
+namespace PRONEXEL_WEB.Controllers
+{
+    [Authorize(Roles ="ApplicationUser,SuperAdmin,Admin")]
+    public class TrainingModuleController : Controller
+    {
+        private readonly MediaRepo mediaRepo;
+        public TrainingModuleController(MediaRepo mediaRepo)
+        {
+            this.mediaRepo = mediaRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> AllMedia()
+        {
+            var res = await mediaRepo.AllMedia();
+            return View(res);
+        }
+    }
+}
